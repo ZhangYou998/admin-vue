3d52f53cd86226ebe728d772e97e117ef976984c
ZhangYou998 <2677599963@qq.com>
2018-04-23 15:23:36 +0800

    角色分配完成

--- src/components/user-list/script.js
+++ src/components/user-list/script.js
@@ -8,7 +8,7 @@ export default {
       tableData: [], // 表格列表数据
       totalSize: 0, // 总记录数据
       currentPage: 1, // 当前页码
-      pageSize: 3, // 当前每页大小
+      pageSize: 5, // 当前每页大小
       userForm: {
         username: '',
         password: '',
@@ -22,72 +22,128 @@ export default {
       },
       dialogFormVisible: false, // 控制添加用户对话框的显示和隐藏
       dialogEditFormVisible: false, // 控制编辑用户对话框的显示和隐藏
+      // 1. 添加 rules 验证规则
       addUserFormRules: {
         username: [
-          {required: true, message: '请输入用户名', trigger: 'blur'},
-          {min: 3, max: 16, message: '长度在 3 到 16 个字符', trigger: 'blur'}
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          { min: 3, max: 18, message: '长度在 3 到 18 个字符', trigger: 'blur' }
         ],
         password: [
-          {required: true, message: '请输入密码', trigger: 'blur'},
-          {min: 6, max: 16, message: '长度在 6 到 16 个字符', trigger: 'blur'}
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { min: 6, max: 18, message: '长度在 6 到 18 个字符', trigger: 'blur' }
         ],
         email: [
-          {required: true, message: '请输入邮箱', trigger: 'blur'}
+          { required: true, message: '请输入邮箱', trigger: 'blur' }
         ],
         mobile: [
-          {required: true, message: '请输入电话', trigger: 'blur'}
+          { required: true, message: '请输入用户名', trigger: 'blur' }
         ]
-      }
+      },
+      userRoleDialog: false,
+      userRoleForm: {
+        username: '',
+        rid: -1 // 默认 -1 表示没有权限
+      },
+      roleList: []
     }
   },
   methods: {
-    // 当我们点击切换每页大小的时候，分页组件就会触发执行 handleSizeChange 方法
-    // 我们要做的就是在这里拿到用户选择的每页大小，重新发起新的数据请求
+    /**
+     * 处理分页页码大小改变
+     */
+
     async handleSizeChange (pageSize) {
-      // 将用户选择的页码大小存储起来
-      // 用于页码改变之后，再次点击页码的时候获取最新用户选择的每页大小
       this.pageSize = pageSize
-      // 页码发生改变 重新请求列表数据
-      // 用户改变每页大小之后，我们请求新的数据，以新的每页大小的第1页为准
       this.loadUsersByPage(1, pageSize)
 
-      // 页码改变之后，不仅让数据到了第1页，同时也要让页码高亮状态跑到第1页
+      // 每页大小改变之后，数据回归到了第1页
+      // 我们的页码的高亮状态也应用回归到第1页
+      // 我们这里就可以使用 this.currentPage 来控制
       this.currentPage = 1
     },
-    // 当你点击分页组件的页码发生变化的时候，会自动触发调用 handleCurrentChange 方法
-    // 我们要做的就是在这个方法中调用 loadUsersByPage(当前页码)
+
+    /**
+     * 处理分页页码改变
+     */
+
     async handleCurrentChange (currentPage) {
-      // 拿到当前最新的每页大小
       this.loadUsersByPage(currentPage)
     },
-    handleSwarch () {
+
+    /**
+     * 处理用户搜索
+     */
+
+    handleSearch () {
       this.loadUsersByPage(1)
     },
-    // 根据页码加载用户列表数据
+
+    /**
+     * 分页加载用户列表数据
+     */
+
     async loadUsersByPage (page) {
       const res = await this.$http.get('/users', {
-        params: { // 请求参数，对象会被转换为 k=v&k=v 的格式，然后拼接到请求路径 ? 后面发起请求
+        params: {
           pagenum: page,
           pagesize: this.pageSize,
-          query: this.searchText // 更具搜索文本框的内容来搜索
+          query: this.searchText // 根据搜索文本框的内容来搜索
         }
       })
-
       const {users, total} = res.data.data
 
       this.tableData = users
+
+      // 把真实的总记录交给分页插件
+      // 分页插件会根据总记录数和每页大小自动完成分页效果
       this.totalSize = total
     },
+
+    /**
+     * 处理用户状态改变
+     * @change="handleStateChange(scope.row)"
+     */
+    async handleStateChange (user) {
+      const {id: userId, mg_state: state} = user
+      const res = await this.$http.put(`/users/${userId}/state/${state}`)
+      if (res.data.meta.status === 200) {
+        this.$message({
+          type: 'success',
+          message: `用户状态${state ? '开启' : '禁用'}成功`
+        })
+      }
+    },
+
+    /**
+     * 处理用户状态改变
+     * @change="(val) => {handleStateChange(val, scope.row)}"
+     */
+
+    // async handleStateChange (state, user) {
+    //   const {id: userId} = user
+    //   const res = await this.$http.put(`/users/${userId}/state/${state}`)
+    //   if (res.data.meta.status === 200) {
+    //     this.$message({
+    //       type: 'success',
+    //       message: `用户状态${state ? '开启' : '禁用'}成功`
+    //     })
+    //   }
+    // },
+
+    /**
+     * 处理添加用户
+     */
+
     async handleAddUser () {
       // 1. 获取表单数据
-      // 2. 变淡验证
+      // 2. 表单验证
       // 3. 发起请求添加用户
       // 4. 根据响应做交互
       //    添加用户成功，给出提示
       //    关闭对话框
       //    重新加载当前列表数据
-      this.$refs['addUserForm'].validate(async (valib) => {
-        if (!valib) {
+      this.$refs['addUserForm'].validate(async (valid) => {
+        if (!valid) {
           return false
         }
         // 代码执行到这里就表示表单验证通过了，我们可以提交表单了
@@ -96,7 +152,7 @@ export default {
           // 添加成功提示消息
           this.$message({
             type: 'success',
-            message: '添加成功'
+            message: '添加用户成功'
           })
 
           // 关闭对话框
@@ -112,8 +168,13 @@ export default {
         }
       })
     },
+
+    /**
+     * 处理删除用户
+     */
+
     async handleDeleteUser (user) {
-      this.$confirm('此操作将永久删除该用户，是否继续?', '提示', {
+      this.$confirm('此操作将永久删除该用户, 是否继续?', '提示', {
         confirmButtonText: '确定',
         cancelButtonText: '取消',
         type: 'warning'
@@ -134,22 +195,67 @@ export default {
         })
       })
     },
+
+    /**
+     * 处理编辑用户
+     */
     async handleEditUser () {
       const {id: userId} = this.editUserForm
       const res = await this.$http.put(`/users/${userId}`, this.editUserForm)
-      if (res.date.meta.status === 200) {
+      if (res.data.meta.status === 200) {
         this.$message({
           type: 'success',
           message: '更新用户成功'
         })
         this.dialogEditFormVisible = false // 关闭编辑用户表单对话框
-        this.loadUsersByPage(this.currentPage) // 重新加载当前页面数据
+        this.loadUsersByPage(this.currentPage) // 重新加载当前页数据
       }
     },
+
+    /**
+     * 处理显示被编辑的用户表单信息
+     */
     async handleShowEditForm (user) {
       this.dialogEditFormVisible = true
       const res = await this.$http.get(`/users/${user.id}`)
       this.editUserForm = res.data.data
+    },
+
+    /**
+     * 《显示分配角色弹框》
+     */
+
+    async handleShowRole (user) {
+      // 发送请求，拿到用户用户
+      // 发送请求，拿到角色列表
+      const res = await this.$http.get(`/users/${user.id}`)
+      const roleRes = await this.$http.get('/roles')
+      const {data, meta} = res.data
+      if (meta.status === 200 && roleRes.data.meta.status === 200) {
+        this.roleList = roleRes.data.data // 更新角色列表
+        this.userRoleForm = data // 更新分配角色表单
+        this.userRoleDialog = true // 弹出对话框
+      }
+    },
+
+    /**
+     * 修改用户角色
+     */
+
+    async handleEditUserRole () {
+      // 得到用户 id 以及当前用户选择的角色 id
+      const {id: userId, rid: roleId} = this.userRoleForm
+      const res = await this.$http.put(`users/${userId}/role`, {
+        rid: roleId
+      })
+      const {data, meta} = res.data
+      if (meta.status === 200) {
+        this.$message({
+          type: 'success',
+          message: '设置角色成功'
+        })
+        this.userRoleDialog = false // 关闭对话框
+      }
     }
   }
 }
