747bb5bcbcce2e6dd81937ec561c858c7024e08e
ZhangYou998 <2677599963@qq.com>
2018-04-23 19:05:30 +0800

    配置完成商品分类列表展示在表格树中-死数据

--- src/main.js
+++ src/main.js
@@ -6,11 +6,15 @@ import 'element-ui/lib/theme-chalk/index.css'
 import App from './App'
 import router from './router'
 import httpPlugin from '@/assets/js/http'
+import ElTreeGrid from 'element-tree-grid'
 
 // 引入公共样式
 import './assets/css/style.css'
 
 Vue.use(ElementUI)
+
+// 将 ElTreeGrid 注册为全局组件
+Vue.component(ElTreeGrid.name, ElTreeGrid)
 
 // 加载 httpPlugin 插件(封装自 axios)
 // 我们在该插件中为 Vue 原型对象扩展了一个成员 $http
